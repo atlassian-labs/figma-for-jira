b66dc25414f1ec3cca1fef658d43bf3aa39e1701
Justin Venkitachalam <jvenkitachalam@atlassian.com>
2023-09-04 11:24:25 +0800

    MDTZ-921 Don't pass both 'useLevel' and 'customLogLevel' options to HTTP logger

--- src/web/middleware/http-logger-middleware.ts
+++ src/web/middleware/http-logger-middleware.ts
@@ -2,7 +2,6 @@ import { pinoHttp } from 'pino-http';
 
 import { getLogger } from '../../infrastructure';
 
-const isTest = process.env.NODE_ENV === 'test';
 export const httpLoggerMiddleware = pinoHttp({
 	logger: getLogger(),
 	autoLogging: {
@@ -11,11 +10,13 @@ export const httpLoggerMiddleware = pinoHttp({
 		},
 	},
 	customLogLevel(req, res) {
+		if (process.env.NODE_ENV === 'test') {
+			return 'silent';
+		}
 		if (res.statusCode >= 500 && res.statusCode < 600) {
 			return 'error';
 		}
 
 		return 'info';
 	},
-	...(isTest && { useLevel: 'silent' }),
 });
