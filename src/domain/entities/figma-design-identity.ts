165c1dd83080aa33baf1a4d8ff53096736f45708
Alexey Kostevich <akostevich@atlassian.com>
2023-09-15 15:35:44 +1000

    MDTZ-970: Improve code comments for FigmaDesignIdentity

--- src/domain/entities/figma-design-identity.ts
+++ src/domain/entities/figma-design-identity.ts
@@ -7,7 +7,7 @@ export class FigmaDesignIdentity {
 	) {}
 
 	/**
-	 * Parses a design identifier into a tuple of its parts: [fileKey, nodeId]
+	 * Creates {@link FigmaDesignIdentity} from the given {@link AtlassianDesign} ID.
 	 */
 	static fromAtlassianDesignId = (id: string): FigmaDesignIdentity => {
 		const [fileKey, nodeId] = id.split('/');
@@ -17,6 +17,11 @@ export class FigmaDesignIdentity {
 		return new FigmaDesignIdentity(fileKey, nodeId);
 	};
 
+	/**
+	 * Creates {@link FigmaDesignIdentity} from the given Figma's design URL.
+	 *
+	 * The method parses the URL and extracts data required for building {@link FigmaDesignIdentity}.
+	 */
 	static fromFigmaDesignUrl = (url: string): FigmaDesignIdentity => {
 		const parsedUrl = new URL(url);
 
@@ -31,13 +36,19 @@ export class FigmaDesignIdentity {
 		return new FigmaDesignIdentity(fileKey, nodeId);
 	};
 
+	/**
+	 * Returns the {@link nodeId} or the document ID (`0:0`).
+	 */
 	get nodeIdOrDefaultDocumentId(): string {
 		return this.nodeId ?? FIGMA_DOCUMENT_NODE_ID;
 	}
 
 	/**
-	 * Builds an {@link AtlassianDesign} identifier given a fileKey and optional nodeId.
-	 * A design identifier is a composite of `<fileKey>/<nodeId>`
+	 * Returns an {@link AtlassianDesign} that corresponds to this {@link FigmaDesignIdentity}.
+	 *
+	 * An ID has the following format:
+	 * - `fileKey/nodeId` when {@link nodeId} is defined.
+	 * - `fileKey` when {@link nodeId} is not defined.
 	 */
 	toAtlassianDesignId = () => {
 		return [this.fileKey, this.nodeId].filter(Boolean).join('/');
