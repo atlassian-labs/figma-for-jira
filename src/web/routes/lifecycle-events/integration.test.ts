5ca9edffaee828b0659c83fac750d0ed4cb5b010
Alexey Kostevich <akostevich@atlassian.com>
2023-09-27 11:31:16 +1000

    MDTZ-993: Add integration tests for lifecycle events

--- src/web/routes/lifecycle-events/integration.test.ts
+++ src/web/routes/lifecycle-events/integration.test.ts
@@ -3,6 +3,8 @@ import nock from 'nock';
 import request from 'supertest';
 import { v4 as uuidv4 } from 'uuid';
 
+import { generateConnectLifecycleRequest } from './testing';
+
 import app from '../../../app';
 import { getConfig } from '../../../config';
 import {
@@ -15,18 +17,6 @@ import {
 	figmaTeamRepository,
 } from '../../../infrastructure/repositories';
 import { generateInboundRequestAsymmetricJwtToken } from '../../testing';
-
-const generateUninstalledRequest = ({
-	key = uuidv4(),
-	clientKey = uuidv4(),
-	sharedSecret = uuidv4(),
-	baseUrl = `https://${uuidv4()}.atlassian.com`,
-}) => ({
-	key,
-	clientKey,
-	sharedSecret,
-	baseUrl,
-});
 
 const mockConnectKeyEndpoint = ({
 	baseUrl,
@@ -64,9 +54,76 @@ const mockFigmaDeleteWebhookEndpoint = ({
 
 describe('/lifecycleEvents', () => {
 	describe('/installed', () => {
+		it('should create a connect installation record', async () => {
+			const clientKey = uuidv4();
+			const installedRequest = generateConnectLifecycleRequest({ clientKey });
+			const keyId = uuidv4();
+			const { jwtToken, publicKey } =
+				await generateInboundRequestAsymmetricJwtToken({
+					keyId,
+					method: 'POST',
+					pathname: '/lifecycleEvents/installed',
+					connectInstallation: {
+						baseUrl: getConfig().app.baseUrl,
+						clientKey,
+					},
+				});
+
+			mockConnectKeyEndpoint({
+				baseUrl: getConfig().jira.connectKeyServerUrl,
+				keyId,
+				publicKey,
+				status: HttpStatusCode.Ok,
+			});
+
+			await request(app)
+				.post('/lifecycleEvents/installed')
+				.set('Authorization', `JWT ${jwtToken}`)
+				.send(installedRequest)
+				.expect(HttpStatusCode.NoContent);
+			expect(
+				await connectInstallationRepository.getByClientKey(clientKey),
+			).toEqual({
+				id: expect.anything(),
+				key: installedRequest.key,
+				clientKey: installedRequest.clientKey,
+				sharedSecret: installedRequest.sharedSecret,
+				baseUrl: installedRequest.baseUrl,
+				displayUrl: installedRequest.displayUrl,
+			});
+		});
+
+		it('should respond 401 when JWT token is invalid (unknown issuer)', async () => {
+			const keyId = uuidv4();
+			const { jwtToken, publicKey } =
+				await generateInboundRequestAsymmetricJwtToken({
+					keyId,
+					method: 'POST',
+					pathname: '/incorrect-pathname',
+					connectInstallation: {
+						baseUrl: getConfig().app.baseUrl,
+						clientKey: uuidv4(),
+					},
+				});
+
+			mockConnectKeyEndpoint({
+				baseUrl: getConfig().jira.connectKeyServerUrl,
+				keyId,
+				publicKey,
+				status: HttpStatusCode.Ok,
+			});
+
+			return request(app)
+				.post('/lifecycleEvents/installed')
+				.set('Authorization', `JWT ${jwtToken}`)
+				.send(generateConnectLifecycleRequest())
+				.expect(HttpStatusCode.Unauthorized);
+		});
+
 		it('should respond 401 when JWT token is missing', () => {
 			return request(app)
 				.post('/lifecycleEvents/installed')
+				.send(generateConnectLifecycleRequest())
 				.expect(HttpStatusCode.Unauthorized);
 		});
 	});
@@ -136,7 +193,7 @@ describe('/lifecycleEvents', () => {
 				.post('/lifecycleEvents/uninstalled')
 				.set('Authorization', `JWT ${jwtToken}`)
 				.send(
-					generateUninstalledRequest({
+					generateConnectLifecycleRequest({
 						key: getConfig().app.key,
 						clientKey: targetConnectInstallation.clientKey,
 					}),
@@ -149,9 +206,37 @@ describe('/lifecycleEvents', () => {
 			).toEqual([]);
 		});
 
+		it('should respond 401 when JWT token is invalid (unknown issuer)', async () => {
+			const keyId = uuidv4();
+			const { jwtToken, publicKey } =
+				await generateInboundRequestAsymmetricJwtToken({
+					keyId,
+					method: 'POST',
+					pathname: '/incorrect-pathname',
+					connectInstallation: {
+						baseUrl: getConfig().app.baseUrl,
+						clientKey: uuidv4(),
+					},
+				});
+
+			mockConnectKeyEndpoint({
+				baseUrl: getConfig().jira.connectKeyServerUrl,
+				keyId,
+				publicKey,
+				status: HttpStatusCode.Ok,
+			});
+
+			return request(app)
+				.post('/lifecycleEvents/uninstalled')
+				.set('Authorization', `JWT ${jwtToken}`)
+				.send(generateConnectLifecycleRequest())
+				.expect(HttpStatusCode.Unauthorized);
+		});
+
 		it('should respond 401 when JWT token is missing', () => {
 			return request(app)
 				.post('/lifecycleEvents/uninstalled')
+				.send(generateConnectLifecycleRequest())
 				.expect(HttpStatusCode.Unauthorized);
 		});
 	});
