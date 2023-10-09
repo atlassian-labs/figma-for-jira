0a18426e864dda3c7fc1a82608719721c8700b69
Alexey Kostevich <akostevich@atlassian.com>
2023-10-09 14:54:24 +1100

    MDTZ-1042: Refactor auth middlewares

--- src/web/middleware/auth/index.ts
+++ src/web/middleware/auth/index.ts
@@ -1,2 +1,2 @@
 export * from './connect-context-symmetric-jwt-auth-middleware';
-export * from './connect-server-symmetric-jwt-auth-middleware';
+export * from './connect-asymmetric-jwt-auth-middleware';
