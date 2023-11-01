b0cb9bcb7e10541a1389ed4eff05e9adeac4ea58
Elaine Lin <elaine.lin@figma.com>
2023-11-01 16:38:35 -0700

    Add comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# This supports both the old environment variables and new ones specific to the Figma setup.
 if [ -n "$PG_FIGMA_FOR_JIRA_DB_ROLE" ]; then
   DATABASE_URL=postgresql://${PG_FIGMA_FOR_JIRA_DB_ROLE}:${PG_FIGMA_FOR_JIRA_DB_PASSWORD}@${PG_FIGMA_FOR_JIRA_DB_HOST}:${PG_FIGMA_FOR_JIRA_DB_PORT}/${PG_FIGMA_FOR_JIRA_DB_SCHEMA}
 else
