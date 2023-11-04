3e8ab1a52c4cd9a929a5ac4f8bf5cbd3c60df15c
Elaine Lin <elaine.lin@figma.com>
2023-11-03 17:25:33 -0700

    Add entrypoint password encoding

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-set -e
+# Function to URL-encode a string
+urlencode() {
+  local string="$1"
+  local encoded=""
+  local length="${#string}"
+  for ((i = 0; i < length; i++)); do
+    local char="${string:i:1}"
+    case "$char" in
+    [a-zA-Z0-9.~_-]) encoded+="$char" ;;
+    *) encoded+="%"$(printf "%02X" "'$char") ;;
+    esac
+  done
+  echo "$encoded"
+}
 
 # This supports both the old environment variables and new ones specific to the Figma setup.
 if [ -n "$PG_FIGMA_FOR_JIRA_DB_ROLE" ]; then
-  DATABASE_URL=postgresql://${PG_FIGMA_FOR_JIRA_DB_ROLE}:${PG_FIGMA_FOR_JIRA_DB_PASSWORD}@${PG_FIGMA_FOR_JIRA_DB_HOST}:${PG_FIGMA_FOR_JIRA_DB_PORT}/${PG_FIGMA_FOR_JIRA_DB_SCHEMA}
+  DATABASE_URL="postgresql://${PG_FIGMA_FOR_JIRA_DB_ROLE}:${PG_FIGMA_FOR_JIRA_DB_PASSWORD}@${PG_FIGMA_FOR_JIRA_DB_HOST}:${PG_FIGMA_FOR_JIRA_DB_PORT}/${PG_FIGMA_FOR_JIRA_DB_SCHEMA}"
 else
-  DATABASE_URL=postgresql://${PG_INTEGRATIONS_JIRA_USER}:${PG_INTEGRATIONS_JIRA_PASSWORD}@${PG_INTEGRATIONS_HOST}:${PG_INTEGRATIONS_PORT}/${PG_INTEGRATIONS_DATABASE}
+  ENCODED_PASSWORD="$(urlencode "$PG_INTEGRATIONS_JIRA_PASSWORD")"
+  DATABASE_URL="postgresql://${PG_INTEGRATIONS_JIRA_USER}:${ENCODED_PASSWORD}@${PG_INTEGRATIONS_HOST}:${PG_INTEGRATIONS_PORT}/${PG_INTEGRATIONS_DATABASE}"
 fi
 
 export DATABASE_URL
