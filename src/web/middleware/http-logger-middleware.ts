f7074a2e879e6f0dab11671db781e115cdbb7adc
Alexey Kostevich <141381034+akostevich-atlassian@users.noreply.github.com>
2023-11-15 08:27:09 +1100

    NONE: Exclude sensitive information from HTTP request/response logs (#167)

--- src/web/middleware/http-logger-middleware.ts
+++ src/web/middleware/http-logger-middleware.ts
@@ -1,9 +1,24 @@
+import type { SerializedRequest, SerializedResponse } from 'pino';
 import { pinoHttp } from 'pino-http';
 
 import { getLogger } from '../../infrastructure';
 
 export const httpLoggerMiddleware = pinoHttp({
 	logger: getLogger(),
+	// Exclude potentially sensitive information from logs (e.g., HTTP headers, remote address, etc.).
+	// See for more detail:
+	// - https://github.com/pinojs/pino-http#custom-serializers
+	// - https://github.com/pinojs/pino-http/issues/5
+	serializers: {
+		req: (req: SerializedRequest) => ({
+			id: req.id,
+			method: req.method,
+			url: req.url,
+		}),
+		res: (res: SerializedResponse) => ({
+			statusCode: res.statusCode,
+		}),
+	},
 	autoLogging: {
 		ignore(req) {
 			return req.url === '/healthcheck';
