6d2b268a2e10a13b5edd76b35cb1494b5603859a
Elaine Lin <elaine.lin@figma.com>
2023-11-17 14:05:49 -0800

    Tweak tsconfig

--- admin/src/utils/parseTeamIdFromFigmaUrl.test.ts
+++ admin/src/utils/parseTeamIdFromFigmaUrl.test.ts
@@ -1,6 +1,7 @@
 import { parseTeamIdFromFigmaUrl } from './parseTeamIdFromFigmaUrl';
 
 describe('parseTeamIdFromFigmaUrl', () => {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 	it('throws an error if the URL is not a Figma URL', () => {
 		const url = 'https://foo.com';
 		expect(() => parseTeamIdFromFigmaUrl(url)).toThrowError(
@@ -8,11 +9,13 @@ describe('parseTeamIdFromFigmaUrl', () => {
 		);
 	});
 
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 	it('returns the team ID for a starter team', () => {
 		const url = 'https://www.figma.com/files/team/123/Project-Name';
 		expect(parseTeamIdFromFigmaUrl(url)).toEqual('123');
 	});
 
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 	it('returns the team ID for an org team', () => {
 		const url =
 			'https://www.figma.com/files/456/team/123/another-cool-team?fuid=678';
