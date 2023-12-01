2dafc5cf3a516d39dd5fe86f8b18fce4074a7502
Alexey Kostevich <akostevich@atlassian.com>
2023-12-01 21:48:58 +1100

    FABB-69: Add tests

--- src/usecases/associate-design-use-case.test.ts
+++ src/usecases/associate-design-use-case.test.ts
@@ -16,6 +16,7 @@ import {
 	generateJiraIssue,
 } from '../domain/entities/testing';
 import { figmaService } from '../infrastructure/figma';
+import { figmaBackfillService } from '../infrastructure/figma/figma-backfill-service';
 import { jiraService } from '../infrastructure/jira';
 import { associatedFigmaDesignRepository } from '../infrastructure/repositories';
 
@@ -140,5 +141,76 @@ describe('associateDesignUseCase', () => {
 			associateDesignUseCase.execute(params),
 		).rejects.toThrow();
 		expect(associatedFigmaDesignRepository.upsert).not.toHaveBeenCalled();
+	});
+
+	it('should construct design from URL when design is being backfilled and it is not found in Figma', async () => {
+		const connectInstallation = generateConnectInstallation();
+		const issue = generateJiraIssue();
+		const fileKey = generateFigmaFileKey();
+		const designId = new FigmaDesignIdentifier(fileKey);
+		const designUrl = new URL(
+			`https://www.figma.com/file/${fileKey}/Design1?com.atlassian.designs.backfill=true`,
+		);
+		const minimalAtlassianDesign =
+			figmaBackfillService.buildMinimalDesignFromUrl(designUrl);
+		const params: AssociateDesignUseCaseParams =
+			generateAssociateDesignUseCaseParams({
+				entityUrl: designUrl.toString(),
+				issueId: issue.id,
+				connectInstallation,
+			});
+		jest.spyOn(figmaService, 'getDesignOrParent').mockResolvedValue(null);
+		jest.spyOn(jiraService, 'getIssue').mockResolvedValue(issue);
+		jest.spyOn(jiraService, 'submitDesign').mockResolvedValue();
+		jest
+			.spyOn(jiraService, 'saveDesignUrlInIssueProperties')
+			.mockResolvedValue();
+		jest
+			.spyOn(figmaService, 'tryCreateDevResourceForJiraIssue')
+			.mockResolvedValue();
+		jest
+			.spyOn(associatedFigmaDesignRepository, 'upsert')
+			.mockResolvedValue({} as AssociatedFigmaDesign);
+
+		await associateDesignUseCase.execute(params);
+
+		expect(figmaService.getDesignOrParent).toHaveBeenCalledWith(designId, {
+			atlassianUserId: params.atlassianUserId,
+			connectInstallationId: params.connectInstallation.id,
+		});
+		expect(jiraService.submitDesign).toHaveBeenCalledWith(
+			{
+				design: minimalAtlassianDesign,
+				addAssociations: [
+					AtlassianAssociation.createDesignIssueAssociation(
+						params.associateWith.ari,
+					),
+				],
+			},
+			connectInstallation,
+		);
+		expect(jiraService.saveDesignUrlInIssueProperties).toHaveBeenCalledWith(
+			issue.id,
+			designId,
+			minimalAtlassianDesign,
+			connectInstallation,
+		);
+		expect(figmaService.tryCreateDevResourceForJiraIssue).toHaveBeenCalledWith({
+			designId,
+			issue: {
+				key: issue.key,
+				title: issue.fields.summary,
+				url: `${connectInstallation.baseUrl}/browse/${issue.key}`,
+			},
+			user: {
+				atlassianUserId: params.atlassianUserId,
+				connectInstallationId: params.connectInstallation.id,
+			},
+		});
+		expect(associatedFigmaDesignRepository.upsert).toHaveBeenCalledWith({
+			designId,
+			associatedWithAri: params.associateWith.ari,
+			connectInstallationId: connectInstallation.id,
+		});
 	});
 });
