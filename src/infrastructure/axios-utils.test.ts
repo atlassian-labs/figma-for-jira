d8a176603f32e341691bbfd2aa46d6fc59a8ca6a
Elaine Lin <elaine.lin@figma.com>
2024-01-08 16:45:59 -0800

    Fix tests

--- src/infrastructure/axios-utils.test.ts
+++ src/infrastructure/axios-utils.test.ts
@@ -1,6 +1,14 @@
+import type { AxiosResponse } from 'axios';
 import { AxiosError } from 'axios';
 
 import { withAxiosErrorTranslation } from './axios-utils';
+import {
+	BadRequestHttpClientError,
+	ForbiddenHttpClientError,
+	HttpClientError,
+	NotFoundHttpClientError,
+	UnauthorizedHttpClientError,
+} from './http-client-errors';
 
 describe('Axios utils', () => {
 	describe('withAxiosErrorTranslation', () => {
@@ -12,44 +20,31 @@ describe('Axios utils', () => {
 			await expect(result).rejects.toThrow('test error');
 		});
 
-		it('can throw a 400 bad request error', async () => {
-			const result = withAxiosErrorTranslation(() => {
-				throw new AxiosError('Bad request', '400');
-			});
+		it.each([
+			[BadRequestHttpClientError, 'Bad request', 400],
+			[UnauthorizedHttpClientError, 'Unauthorized', 401],
+			[ForbiddenHttpClientError, 'Forbidden', 403],
+			[NotFoundHttpClientError, 'Not found', 404],
+			[HttpClientError, 'Internal Server Error', 500],
+		])(
+			'should throw %s when Axios error with HTTP status %s is caught',
+			async (ErrorType, message, status) => {
+				const axiosError = new AxiosError(
+					message,
+					undefined,
+					undefined,
+					undefined,
+					{
+						status,
+					} as AxiosResponse,
+				);
 
-			await expect(result).rejects.toThrow('Bad request');
-		});
+				const result = withAxiosErrorTranslation(() => {
+					throw axiosError;
+				});
 
-		it('can throw a 401 unauthorized error', async () => {
-			const result = withAxiosErrorTranslation(() => {
-				throw new AxiosError('Unauthorized', '401');
-			});
-
-			await expect(result).rejects.toThrow('Unauthorized');
-		});
-
-		it('can throw a 403 forbidden error', async () => {
-			const result = withAxiosErrorTranslation(() => {
-				throw new AxiosError('Forbidden', '403');
-			});
-
-			await expect(result).rejects.toThrow('Forbidden');
-		});
-
-		it('can throw a 404 not found error', async () => {
-			const result = withAxiosErrorTranslation(() => {
-				throw new AxiosError('Not Found', '404');
-			});
-
-			await expect(result).rejects.toThrow('Not Found');
-		});
-
-		it('can throw a generic http client error', async () => {
-			const result = withAxiosErrorTranslation(() => {
-				throw new AxiosError('Internal Server Error', '500');
-			});
-
-			await expect(result).rejects.toThrow('Internal Server Error');
-		});
+				await expect(result).rejects.toBeInstanceOf(ErrorType);
+			},
+		);
 	});
 });
