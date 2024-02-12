2648305689b6dd820fc644851c002c24bdcb8d7a
Alexey Kostevich <akostevich@atlassian.com>
2024-02-12 14:09:46 +1100

    MDTZ-1461: Fix an issue with the Issue Panel being unavailable

--- src/web/middleware/jira/jira-context-symmetric-jwt-from-query-authentication-middleware.ts
+++ src/web/middleware/jira/jira-context-symmetric-jwt-from-query-authentication-middleware.ts
@@ -1,10 +1,12 @@
+import { fromExpressRequest } from 'atlassian-jwt';
 import type { NextFunction, Request, RequestHandler, Response } from 'express';
 
-import { jiraContextSymmetricJwtTokenVerifier } from '../../../infrastructure/jira/inbound-auth';
+import { isString } from '../../../common/string-utils';
+import { jiraIframeOrServerToServerSymmetricJwtTokenVerifier } from '../../../infrastructure/jira/inbound-auth';
 import { UnauthorizedResponseStatusError } from '../../errors';
 
 /**
- * Authenticates requests using a symmetric JWT token received in a query param.
+ * Authenticates requests using an iframe symmetric JWT token received in a query param.
  *
  * In case of successful authentication, `connectInstallation` and `atlassianUserId` are set in locals.
  *
@@ -14,24 +16,29 @@ import { UnauthorizedResponseStatusError } from '../../errors';
  * @see https://developer.atlassian.com/cloud/jira/platform/understanding-jwt-for-connect-apps/#types-of-jwt-token
  * @see https://community.developer.atlassian.com/t/action-required-atlassian-connect-vulnerability-allows-bypass-of-app-qsh-verification-via-context-jwts/47072
  */
-export const jiraContextSymmetricJwtFromQueryAuthenticationMiddleware: RequestHandler =
-	(req: Request, res: Response, next: NextFunction) => {
-		const token = req.params.jwt;
+export const jiraIframeSymmetricJwtAuthenticationMiddleware: RequestHandler = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	const token = req.query.jwt;
 
-		if (!token) {
-			return next(new UnauthorizedResponseStatusError('Missing JWT token.'));
-		}
+	if (!isString(token) || !token) {
+		return next(new UnauthorizedResponseStatusError('Missing JWT token.'));
+	}
 
-		void jiraContextSymmetricJwtTokenVerifier
-			.verify(token)
-			.then(({ connectInstallation, atlassianUserId }) => {
-				res.locals.connectInstallation = connectInstallation;
-				res.locals.atlassianUserId = atlassianUserId;
-				next();
-			})
-			.catch((e) =>
-				next(
-					new UnauthorizedResponseStatusError('Unauthorized.', undefined, e),
-				),
-			);
-	};
+	void jiraIframeOrServerToServerSymmetricJwtTokenVerifier
+		.verify(token, fromExpressRequest(req))
+		.then(({ connectInstallation, atlassianUserId }) => {
+			if (!atlassianUserId) {
+				return next(new UnauthorizedResponseStatusError('Unauthorized.'));
+			}
+
+			res.locals.connectInstallation = connectInstallation;
+			res.locals.atlassianUserId = atlassianUserId;
+			next();
+		})
+		.catch((e) =>
+			next(new UnauthorizedResponseStatusError('Unauthorized.', undefined, e)),
+		);
+};
--- src/web/routes/static/static-router.ts
+++ src/web/routes/static/static-router.ts
@@ -2,23 +2,7 @@ import { Router, static as Static } from 'express';
 
 import { join } from 'path';
 
-import {
-	jiraAdminOnlyAuthorizationMiddleware,
-	jiraContextSymmetricJwtFromQueryAuthenticationMiddleware,
-} from '../../middleware/jira';
-
 export const staticRouter = Router();
-
-staticRouter.use(
-	'/admin/index.html',
-	jiraContextSymmetricJwtFromQueryAuthenticationMiddleware,
-	jiraAdminOnlyAuthorizationMiddleware,
-);
-
-staticRouter.use(
-	'/issue-panel/issue-panel.html',
-	jiraContextSymmetricJwtFromQueryAuthenticationMiddleware,
-);
 
 // Static resources
 staticRouter.use('/admin', Static(join(process.cwd(), 'admin/dist')));
