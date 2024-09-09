e4a40f15f40a737119b003bd9a840f4ae8fbe45c
Roy Fu <rfu@figma.com>
2024-09-09 10:35:07 -0400

    NONE: handle when the issue has been deleted or the user is not able to view the issue (#245)

--- src/infrastructure/jira/errors.ts
+++ src/infrastructure/jira/errors.ts
@@ -1,3 +1,4 @@
 import { CauseAwareError } from '../../common/errors';
 
 export class ForbiddenByJiraServiceError extends CauseAwareError {}
+export class NotFoundByJiraServiceError extends CauseAwareError {}
