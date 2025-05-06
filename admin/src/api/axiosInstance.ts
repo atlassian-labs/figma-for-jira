288442c8008e7009d59cef3857b21fa29dbb5fda
Khanh Nguyen <knguyen@figma.com>
2025-05-05 22:15:09 -0400

    nit: remove undefined fallback

--- admin/src/api/axiosInstance.ts
+++ admin/src/api/axiosInstance.ts
@@ -4,7 +4,7 @@ import { getAppBasePath } from '../utils';
 const THIRTY_SECONDS_IN_MS = 30_000;
 const axiosRest = axios.create({
 	timeout: THIRTY_SECONDS_IN_MS,
-	baseURL: getAppBasePath() ?? undefined,
+	baseURL: getAppBasePath(),
 });
 // Adding the token in the headers through interceptors because it is an async value
 axiosRest.interceptors.request.use(async (config) => {
